caa2ea4d5e5a2fab5628173aa0e3047a7489580d
cair71 <cairzz222@gmail.com>
2021-05-18 20:44:58 +0100

    add troubleMessage if client have a trouble

--- web/src/components/pages/Loader.tsx
+++ web/src/components/pages/Loader.tsx
@@ -1,23 +1,58 @@
-import React, { FC, ReactElement } from "react";
-
+import React, { FC, ReactElement, useEffect } from "react";
 import logo from "../../assets/melyon-animation.svg";
-
 import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { LoaderState } from "../../redux/reducers/general/loader";
+import { Dispatch, StateInterface } from "../../redux";
+
+const LoaderContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
 
 const LogoStyle = styled.div<{ url: string }>`
   width: 100px;
   height: 100px;
   background: url(${(theme) => theme.url}) no-repeat;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  margin: 0 0 30px auto;
 `;
 
-export const Loader: FC = (): ReactElement => (
-  <div id="loader-screen">
-    <div className="content">
+const Trouble = styled.div<{ isTrouble: boolean }>`
+  display: ${(theme) => (theme.isTrouble ? "inline-block" : "none")};
+  margin: 0 -30px;
+  color: var(--main-hidden-color);
+  font-family: monospace, sans-serif;
+`;
+
+export const Loader: FC = (): ReactElement => {
+  const dispatch = useDispatch<Dispatch<LoaderState>>();
+  const { troubleMessage } = useSelector<StateInterface, LoaderState>(
+    (state) => state.loader
+  );
+  useEffect(() => {
+    const trouble = setTimeout(() => {
+      dispatch({
+        type: "UPDATE_LOADER_STATE",
+        payload: {
+          troubleMessage: "⚠ A connection trouble",
+        },
+      });
+    }, 3000 * 2);
+    return () => {
+      clearTimeout(trouble);
+    };
+  });
+  return (
+    <LoaderContainer>
       <LogoStyle url={logo} />
-    </div>
-  </div>
-);
+      <Trouble
+        isTrouble={troubleMessage && troubleMessage.length !== 0 ? true : false}
+      >
+        {troubleMessage}
+      </Trouble>
+    </LoaderContainer>
+  );
+};
