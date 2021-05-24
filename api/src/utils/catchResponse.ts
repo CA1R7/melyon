e84dd17a3b406e848eca0639743676bba1eaff35
cair71 <cairzz222@gmail.com>
2021-05-24 18:10:49 +0100

    fixed the name of http messages file

--- api/src/utils/catchResponse.ts
+++ api/src/utils/catchResponse.ts
@@ -1,4 +1,4 @@
-import HttpMessagesData from "../data/http-code-messages.json"
+import httpCodeMessagesJson from "../data/http-code-messages.json";
 
 interface CatchOptions {
   status?: boolean;
@@ -11,26 +11,27 @@ interface CatchOptions {
   };
 }
 
-export function catchReponseMessage({
-	code,
-	message,
-	options,
-}: CatchOptions): CatchOptions {
-	let object: CatchOptions = {
-		status: code && code === 200 ? true : false,
-		code: code || 500,
-	}
+export function catchReponseMessage({ code, message, options }: CatchOptions): CatchOptions {
+  let object: CatchOptions = {
+    status: code && code === 200 ? true : false,
+    code: code || 500,
+  };
 
-	let messageError: CatchOptions
-	for (messageError of HttpMessagesData) {
-		if (messageError.code === code) {
-			if (!message || message.length === 0)
-				object.message = messageError.message
-		}
-	}
+  let messageError: CatchOptions;
+  for (messageError of httpCodeMessagesJson) {
+    if (messageError.code === code) {
+      if (!message || message.length === 0) {
+        object.message = messageError.message;
+      }
+    }
+  }
 
-	if (message) object.message = message
-	if (options) object = Object.assign(object, options)
+  if (message) {
+    object.message = message;
+  }
+  if (options) {
+    object = { ...object, ...options };
+  }
 
-	return object
+  return object;
 }
