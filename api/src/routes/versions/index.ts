065ac4d8e16f47762b8ab73ad51ba2681a025864
cair71 <cairzz222@gmail.com>
2021-05-24 18:11:23 +0100

    add now type for extension 'links'

--- api/src/routes/versions/index.ts
+++ api/src/routes/versions/index.ts
@@ -1,8 +1,8 @@
 /*
   versions<index> - route
 */
-import { Request, Response } from "express"
-import { catchReponseMessage } from "../../utils/catchResponse"
+import { Request, Response } from "express";
+import { catchReponseMessage } from "../../utils/catchResponse";
 
 export interface ColorType {
   name_color: string;
@@ -12,28 +12,31 @@ export interface ColorType {
   preview_image: string;
 }
 
+export interface LinkType {
+  name: string;
+  ico: string;
+  href: string;
+}
+
 export interface ExtensionType {
   name: string;
   g_version: string | number;
+  extension_pub: string;
+  badges: string[];
   versions: ColorType[];
+  links: LinkType[];
   features: string[];
   repoGithub: string;
 }
 
-export const extension = async (
-	_req: Request,
-	res: Response
-): Promise<void> => {
+export const extension = async (_req: Request, res: Response): Promise<void> => {
   // eslint-disable-line
-	try {
-		const data: ExtensionType = (await import("../../data/versions-ext.json"))
-			.default,
-			code = 200
-		res.json(
-			catchReponseMessage(Object.assign({}, { code: code, options: data }))
-		)
-	} catch (e) {
-		const code = 400
-		res.json(catchReponseMessage({ code, message: "something went wrong" }))
-	}
-}
+  try {
+    const data: ExtensionType = (await import("../../data/versions-ext.json")).default,
+      code = 200;
+    res.json(catchReponseMessage({ code: code, options: data }));
+  } catch (e) {
+    const code = 400;
+    res.json(catchReponseMessage({ code, message: "something went wrong" }));
+  }
+};
