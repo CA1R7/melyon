b0de878dec6fcc6d57f03a8bc854718a683c58b5
cair71 <cairzz222@gmail.com>
2021-05-24 18:02:22 +0100

    styled to prettier

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,15 @@
-require("dotenv").config() // eslint-disable-line
-import * as bodyParser from "body-parser"
-import express from "express"
-import cors from "cors"
-import routes from "./routes"
+require("dotenv").config(); // eslint-disable-line
+import * as bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import routes from "./routes";
 
-const app: express.Application = express()
-const port = process.env.PORT || 8080
+const app: express.Application = express();
+const port = process.env.PORT || 8080;
 
-
-app.use(bodyParser.json())
-app.use(cors())
-app.use(routes)
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
 app.listen(port, () => {
-	console.info("melyon started on *:" + port)
-})
+  console.info("melyon started on *:" + port);
+});
